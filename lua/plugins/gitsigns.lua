1ec18f2c9e291e9ff8ddb9ab81de758b0d188abb
fenlyin0420 <fenlyin0420@gmail.com>
2026-05-10 00:48:10 +0800

    feat: add keybindings to preview diff and restore current position

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,7 +10,10 @@ return {
                 changedelete = { text = "~" },
                 untracked    = { text = "┆" },
             },
-            current_line_blame = true, -- 行尾显示 Git 提交信息（可选）
+            current_line_blame = true, -- 行尾显示 Git 提交信息
+
+            vim.keymap.set("n", "<leader>p", "<cmd>Gitsigns preview_hunk_inline<cr>", { desc = "Git: 预览当前位置 diff" });
+            vim.keymap.set("n", "<leader>r", "<cmd>Gitsigns reset_hunk<cr>", { desc = "Git: restore 当前位置" });
         })
     end,
 }
